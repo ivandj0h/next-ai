105a20a25414c0bf0e7057a279d942a6fd10e85b
ivandjoh <ivandi.djoh@gmail.com>
2024-06-21 19:56:42 +0800

    bugfix google provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import TasksComponent from "@/components/todos/tasks";
+import {Button} from "@/components/ui/button";
+import {signInAction} from "../../actions/auth-action";
 
 export default function Home() {
 
     return (
-        <main>
-           <TasksComponent />
-        </main>
+        <>
+            <form action={signInAction}>
+                {/*<TasksComponent />*/}
+                <Button>Login</Button>
+            </form>
+        </>
     );
 }
