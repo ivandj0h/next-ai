51a9980ca290e5fc4322f1cd94ef865450a46943
ivandjoh <ivandi.djoh@gmail.com>
2024-06-21 18:30:29 +0800

    setup convex provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import TasksComponent from "@/components/todos/tasks";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    </main>
-  );
+
+    return (
+        <main>
+           <TasksComponent />
+        </main>
+    );
 }
